00ec5747d8e348ad873f2730d58db5bd57dbd850
Alex Shishkin <alexsischin@gmail.com>
2023-02-13 15:46:41 +0200

    Removed size check from append_series routine

--- feature.py
+++ feature.py
@@ -5,10 +5,6 @@ from pandas import Series, DataFrame
 
 
 def append_series(df: DataFrame, *args: Series) -> DataFrame:
-    rows = df.shape[0]
-    for a in args:
-        if a.size != rows:
-            raise ValueError(f'All series must have the same length as dataframe. Got: {a.size} ({a.name})')
     return pd.concat([df, pd.concat(args, axis=1, ignore_index=True)], axis=1, ignore_index=True)
 
 
